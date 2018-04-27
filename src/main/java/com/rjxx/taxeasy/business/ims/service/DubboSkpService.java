0ad2801164b834c6a043a0cf39404b8e146786ee
许黎明 <1084854604@qq.com>
2018-04-27 20:54:35 +0800

    添加dubboService

---
+++ src/main/java/com/rjxx/taxeasy/business/ims/service/DubboSkpService.java
@@ -0,0 +1,32 @@
+package com.rjxx.taxeasy.business.ims.service;
+
+/**
+ * @ClassName DubboSkpService
+ * @Description TODO
+ * @Author 许黎明
+ * @Date 2018-04-25 15:20
+ * @Version 1.0
+ **/
+public interface DubboSkpService {
+
+    /**
+     * 凯盈盒子终端授权
+     * @param skpid
+     * @return
+     */
+    public String deviceAuth(int skpid)throws Exception;
+
+    /**
+     * 应用向凯盈平台查询终端状态
+     * @param skpid
+     * @return
+     */
+    public String deviceState(int skpid)throws Exception;
+
+    /**
+     * 应用向凯盈平台设置税控装置密码
+     * @param skpid
+     * @return
+     */
+    public String inputUDiskPassword(int skpid)throws Exception;
+}
